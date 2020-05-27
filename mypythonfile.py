e8dbbfe54333283ba487345273ee3cb7ab0a9bfe
Colin Green <Colin@Colins-MacBook-Pro-2.local>
2020-05-27 14:01:45 -0400

    Updated mypythonfile to include code

--- mypythonfile.py
+++ mypythonfile.py
@@ -3,3 +3,11 @@ mypythonfile.py
 
 A program that greets you with the date
 """
+from datetime import date
+today = date.today()
+monthDayYear = today.strftime("%B %d, %Y")
+
+user = raw_input("Please enter your name: ")
+print(user)
+print("Hello " + user)
+print("Today is %s" % monthDayYear)
