9b21612138b28f6dbe1e319d913bc21e337c3376
Colin Green <Colin@Colins-MacBook-Pro-2.local>
2020-05-27 14:08:47 -0400

    Edited code to be compatible with Python3

--- mypythonfile.py
+++ mypythonfile.py
@@ -7,7 +7,7 @@ from datetime import date
 today = date.today()
 monthDayYear = today.strftime("%B %d, %Y")
 
-user = raw_input("Please enter your name: ")
+user = input("Please enter your name: ")
 print(user)
 print("Hello " + user)
 print("Today is %s" % monthDayYear)
